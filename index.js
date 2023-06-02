7567c4a9a4c22d81abe4239884a5c3c0f8e49260
snakydh <skdanielg5@gmail.com>
2023-06-01 21:34:50 -0500

    Solve problems

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from "express";
-import { SerialPort } from "serialport";
+//import { SerialPort } from "serialport";
 import cors from "cors";
 const app = express();
-const port = new SerialPort({ path: "COM3", baudRate: 9600 });
+//const port = new SerialPort({ path: "COM3", baudRate: 9600 });
 const data = [];
 
-port.on("data", (distance) => {
+/* port.on("data", (distance) => {
   let dataInt;
   dataInt = parseInt(distance.toString());
   dataInt !== 0 ? data.push(dataInt) : null;
   console.log(parseInt(distance.toString()));
-});
+}); */
 
+// send a random number and push it to the data array the random number is between 1 and 21
+for (let i = 0; i < 100; i++) {
+  data.push(Math.floor(Math.random() * 21) + 1);
+  if (i % 2 !== 0) {
+    data.push(20);
+  }
+}
+console.log(data);
 app.use(cors());
 app.get("/", (req, res) => {
   setTimeout(() => {
